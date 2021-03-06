3258ae1be23337e974efe1d720fcf47688985ce4
Morgan Schall <morgan.allison.schall@gmail.com>
2021-03-06 16:40:26 -0500

    added html elements for current weather card

---
+++ script.js
@@ -0,0 +1,4 @@
+var currentAPI = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=14bc8790460d5c199a2e1724c8bc204d"
+var fiveDayAPI = "api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid=14bc8790460d5c199a2e1724c8bc204d"
+
+
